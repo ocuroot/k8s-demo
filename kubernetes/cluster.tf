49d66dae2ca504f37a9425e985f24c22430b9f5a
Tom Elliott <tom.w.elliott@gmail.com>
2025-06-29 22:22:05 -0400

    Rename node pool for entropy

--- kubernetes/cluster.tf
+++ kubernetes/cluster.tf
@@ -6,6 +6,6 @@ resource "vultr_kubernetes" "k8" {
     node_pools {
         node_quantity = 1
         plan          = "vc2-1c-2gb"
-        label         = "vke-nodepool"
+        label         = "pool1"
     }
 }
