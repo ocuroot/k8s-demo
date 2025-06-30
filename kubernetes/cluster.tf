306f128095819bc258c4b58da1955c042744de1e
Tom Elliott <tom.w.elliott@gmail.com>
2025-06-29 22:07:48 -0400

    Remove now unneeded directive from k8s config

--- kubernetes/cluster.tf
+++ kubernetes/cluster.tf
@@ -8,11 +8,4 @@ resource "vultr_kubernetes" "k8" {
         plan          = "vc2-1c-1gb"
         label         = "vke-nodepool"
     }
-
-    lifecycle {
-      ignore_changes = [
-        # Don't change the number of nodes after autoscaling
-        node_pools[0].node_quantity
-      ]
-    }
 }
