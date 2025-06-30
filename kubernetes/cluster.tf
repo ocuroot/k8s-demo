7040b6a585f0286f0601e2c6c7152f0ad214a676
Tom Elliott <tom.w.elliott@gmail.com>
2025-06-29 22:13:40 -0400

    Use a valid plan

--- kubernetes/cluster.tf
+++ kubernetes/cluster.tf
@@ -5,7 +5,7 @@ resource "vultr_kubernetes" "k8" {
 
     node_pools {
         node_quantity = 1
-        plan          = "vc2-1c-1gb"
+        plan          = "vc2-1c-2gb"
         label         = "vke-nodepool"
     }
 }
