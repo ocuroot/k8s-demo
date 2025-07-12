269213999bf212ba4cddec7fe9528d6acad13e6e
Tom Elliott <tom.w.elliott@gmail.com>
2025-07-12 16:01:02 -0400

    Bump cluster version to make valid

--- kubernetes/cluster.tf
+++ kubernetes/cluster.tf
@@ -1,8 +1,8 @@
 resource "digitalocean_kubernetes_cluster" "k8" {
     name    = local.env_name
     region  = "nyc1"  # New York region (closest equivalent to Vultr's ewr)
-    version = "1.33.1-do.0"  # Using a recent stable version
-    
+    version = "1.31.9-do.1"  # Using a recent stable version
+
     node_pool {
         name       = "worker-pool-${local.env_name}"
         size       = "s-1vcpu-2gb"  # Equivalent to Vultr's 1c-2gb instance
